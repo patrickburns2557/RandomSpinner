aa560164b559ae9d32e0ad12b025f520ff4b1c1c
Patrick <patrickburns2557@gmail.com>
2024-03-08 15:32:26 -0600

    Removed beginning of list bias to randomization & improved layout

--- RandomChoice.py
+++ RandomChoice.py
@@ -4,22 +4,9 @@ from math import ceil, sqrt
 import tkinter as tk
 import tkinter.ttk as ttk
 
-# choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+OG_BACKGROUND = "#F0F0F0"
+PICKED_BACKGROUND = "yellow"
 
-
-# number = (len(choices)*2)+random.randint(1,len(choices)) #Number of times to loop through given choices list
-# sleepTime = 0.01
-# sleepIncrease = (1.55/(len(choices)))**2 #Increase sleep time between each iteration, to have it slow down as it reaches the final choice
-# currentIndex = 0
-
-# for i in range(number):
-#     time.sleep(sleepTime)
-#     print("Current choice: " + str(choices[currentIndex]).upper())
-#     print("sleep time: " + str(sleepTime))
-#     print()
-#     currentIndex += 1
-#     currentIndex %= len(choices)
-#     sleepTime += sleepIncrease
 
 #tkinter friendly version of time.sleep, from here: https://stackoverflow.com/questions/10393886/tkinter-and-time-sleep/74162322#74162322
 def tksleep(self, time:float) -> None:
@@ -38,49 +25,36 @@ def pickChoice():
     global numChoices
     global sleepTime
     global sleepIncrease
+    
+    sleepTime = 0.01 #reset sleep time so subsequent rolls aren't slower than previous ones
     previousChoice = 0
-    sleepTime = 0.01
-    numberLoops = (numChoices*2)+random.randint(1,numChoices)
     
     #Reset all choices back to default color, in case a roll was already done without restarting program
     for i in range(numChoices):
-        frameList[i].configure(background="#F0F0F0")
+        frameList[i].configure(background=OG_BACKGROUND)
 
-    #visually shuffle through the choices randomly, instead of being like a "spin" in order
+    #shuffle a list twice as long as the number of choices and iterate through it to visually shuffle through the choices
     shuffledList = []
-    for i in range(numberLoops):
+    for i in range(numChoices*2):
         index = i % numChoices
         shuffledList.append(index)
     random.shuffle(shuffledList)
     
 
     for i in shuffledList:
-            frameList[previousChoice].configure(background="#F0F0F0") #only highlight one choice at a time while program is picking
-            frameList[i].configure(background="yellow")
+            frameList[previousChoice].configure(background=OG_BACKGROUND) #reset previous choice to only highlight one choice at a time while program is picking
+            frameList[i].configure(background=PICKED_BACKGROUND)
             previousChoice = i
             window.tksleep(sleepTime)
             sleepTime += sleepIncrease
-    
-    # for i in range(numberLoops):
-    #     # frameList[previousChoice].configure(background="white")
-    #     # picked = random.randint(0, numChoices-1)
-    #     # frameList[picked].configure(background="yellow")
-    #     # previousChoice = picked
-    #     # window.tksleep(0.5)
-    #     currentChoice = (i%(numChoices))
-    #     print(currentChoice)
-
-    #     frameList[previousChoice].configure(background="#F0F0F0")
-    #     frameList[currentChoice].configure(background="yellow")
-    #     previousChoice = currentChoice
-    #     window.tksleep(sleepTime)
-    #     sleepTime += sleepIncrease
     
 
 
 window = tk.Tk()
 window.title("Random")
 window.state("zoomed") #start maximized
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(1, weight=1)
 
 numChoices = 26
 
@@ -94,14 +68,18 @@ frameList = []
 sizeOfGrid = ceil(sqrt(numChoices)) #width and height of grid will be the sqrt of the number of choices, rounded up
 currentRow = 0
 currentColumn = 0
-window.grid_rowconfigure(tuple(range(sizeOfGrid)), weight=1) #set all grid rows and columns to fill the max amount of space
-window.grid_columnconfigure(tuple(range(sizeOfGrid)), weight=1)
 
+
+
+choicesFrame = tk.LabelFrame(window, text="Choices", font=("Roboto", 12),relief=tk.RAISED, borderwidth=5)
+choicesFrame.grid_rowconfigure(tuple(range(sizeOfGrid)), weight=1) #set all grid rows and columns to fill the max amount of space
+choicesFrame.grid_columnconfigure(tuple(range(sizeOfGrid)), weight=1)
+choicesFrame.grid(row=0, column=1, sticky="news", padx=50, pady=30)
 #Create grid of choices
 for i in range(numChoices):
     #frameList.append(tk.Label(window, text="choice " + str(i), font=("Roboto", 15), background=random.choice(["red", "blue", "green"]))) #Create labels with random background colors to easily differentiate borders for now
-    frameList.append(tk.Label(window, text="choice " + str(i), font=("Roboto", 15)))
-    frameList[i].grid(sticky="news", row=currentRow, column=currentColumn)
+    frameList.append(tk.Label(choicesFrame, text="choice " + str(i), font=("Roboto", 15), relief=tk.GROOVE, borderwidth=3))
+    frameList[i].grid(sticky="news", row=currentRow, column=currentColumn, padx=5, pady=5)
     currentColumn += 1 #increase column position for each choice
     
     #if last column reached, set back to first column and start on next row
@@ -109,8 +87,8 @@ for i in range(numChoices):
         currentColumn = 0
         currentRow += 1
 
-button = tk.Button(window, text="Pick", command=lambda: pickChoice())
-button.grid(row=0, column=sizeOfGrid)
+button = tk.Button(window, text="Pick", width=20, relief=tk.GROOVE, borderwidth=5, command=lambda: pickChoice())
+button.grid(row=0, column=0, sticky="ns", padx=30, pady=50)
     
 window.mainloop()
 
